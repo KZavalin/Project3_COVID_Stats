66baade1255a5e629e602d0a8cfa4b450b31f240
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-06-26 20:10:08 -0400

    set up everything but optionChanged

--- H2/app.js
+++ H2/app.js
@@ -24,13 +24,13 @@ function init() {
         });
 
         // building the plots with the first name in the list
-        barPlots(names[0]);
-        barPlots2(names[0])
+        barPlots(names[0], names[0]);
+        // barPlots2(names[0])
     });
 };
 
 // plots the tourism and infection rates of country 1 on the left
-function barPlots(country) {
+function barPlots(country1, country2) {
     d3.json(url).then((data) => {
         // filtering out countries that lack data
         let filteredData = data.filter(entry => {
@@ -43,18 +43,31 @@ function barPlots(country) {
             names.push(filteredData[i].country_name)
         }
 
-        // filtering so that the name matches that of the country
-        let matchedCountries = filteredData.filter((item) => {
+        // filtering so that the name matches that of the country1
+        let matchedCountries1 = filteredData.filter((item) => {
             for (let i = 0; i < filteredData.length; i++) {
-                if (filteredData[i].country_name == country)
-                    return item.country_name == country;
+                if (filteredData[i].country_name == country1)
+                    return item.country_name == country1;
             }
             
         });
 
-        year_2018 = matchedCountries[0].year_2018;
-        year_2019 = matchedCountries[0].year_2019;
-        year_2020 = matchedCountries[0].year_2020;
+        year_2018 = matchedCountries1[0].year_2018;
+        year_2019 = matchedCountries1[0].year_2019;
+        year_2020 = matchedCountries1[0].year_2020;
+        
+        // filtering so that the name matches that of the country2
+        let matchedCountries2 = filteredData.filter((item) => {
+            for (let i = 0; i < filteredData.length; i++) {
+                if (filteredData[i].country_name == country2)
+                    return item.country_name == country2;
+            }
+            
+        });
+
+        year_2018_2 = matchedCountries2[0].year_2018;
+        year_2019_2 = matchedCountries2[0].year_2019;
+        year_2020_2 = matchedCountries2[0].year_2020;
 
         let trace1 = {
             x:[2018, 2019, 2020],
@@ -62,8 +75,8 @@ function barPlots(country) {
             type:"bar",
             name:"Tourism",
             orientation: "v"
-        };
-
+        }; 
+        
         let trace2 = {
             x:[2020],
             y:[matchedCountries[0].total_cases_per_million],
@@ -72,79 +85,37 @@ function barPlots(country) {
             orientation:"v"
         }
 
+        let trace3 = {
+            x:[2018, 2019, 2020],
+            y:[year_2018_2, year_2019_2, year_2020_2],
+            type:"bar",
+            name:"Tourism 2",
+            orientation: "v"
+        };
+        
+        let trace4 = {
+            x:[2020],
+            y:[matchedCountries2[0].total_cases_per_million],
+            name:"Infections 2",
+            type:"bar",
+            orientation:"v"
+        }
+
         let layout = {
-            title: "Tourism and Infection Rates of Country 1"
+            title: "Tourism and Infection Rates"
         };
 
-        traceData = [trace1, trace2]
+        traceData = [trace1, trace2, trace3, trace4]
 
         Plotly.newPlot("bar", traceData, layout);
     });
 };
 
 // changes the country when the dropdown menu changes for country 1
-function optionChanged(country) {
+function optionChanged(country1, country2) {
 
-    console.log("next value:", country);
-    barPlots(country);
-};
-
-// plots the tourism and infection rates of country 2 on the right
-function barPlots2(country) {
-    d3.json(url).then((data) => {
-        // filtering out countries that lack data
-        let filteredData = data.filter(entry => {
-            return entry.year_2018 > 0 && entry.year_2019 > 0 && entry.year_2020 > 0 && entry.total_cases_per_million > 0;
-        });
-
-        let names = []
-        for (let i = 0; i < filteredData.length; i++) {
-            names.push(filteredData[i].country_name)
-        }
-
-        // filtering so that the name matches that of the country
-        let matchedCountries = filteredData.filter((item) => {
-            for (let i = 0; i < filteredData.length; i++) {
-                if (filteredData[i].country_name == country)
-                    return item.country_name == country;
-            }
-            
-        });
-
-        year_2018 = matchedCountries[0].year_2018;
-        year_2019 = matchedCountries[0].year_2019;
-        year_2020 = matchedCountries[0].year_2020;
-
-        let trace1 = {
-            x:[2018, 2019, 2020],
-            y:[year_2018, year_2019, year_2020],
-            type:"bar",
-            name:"Tourism",
-            orientation: "v"
-        };
-
-        let trace2 = {
-            x:[2020],
-            y:[matchedCountries[0].total_cases_per_million],
-            name:"Infections",
-            type:"bar",
-            orientation:"v"
-        }
-
-        let layout = {
-            title: "Tourism and Infection Rates of Country 2"
-        };
-
-        traceData = [trace1, trace2]
-
-        Plotly.newPlot("bar2", traceData, layout);
-    });
-};
-
-// changes the country when the dropdown menu changes for country 2
-function optionChanged2(country) {
-    console.log("next value (2):", country);
-    barPlots2(country);
+    console.log("next values:", country1, "and", country2);
+    barPlot(country1, country2);
 };
 
 init();
